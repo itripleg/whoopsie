e4e9090526ebac045ef6a523522918b3cf1feae7
Joshua Bell <joshua.bell.828@gmail.com>
2024-04-03 17:16:48 -0400

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>😅</text></svg>"
+        />
         <script
           defer
           src="https://analytics.us.umami.is/script.js"
